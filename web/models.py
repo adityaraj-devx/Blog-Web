daf41f7802c8b692686ea1a60b1ff3f0c7ab5cf9
adityaraj-devx <aadiiraj7780@gmail.com>
2026-07-11 20:52:20 +0530

    added feature in posting system

--- web/models.py
+++ web/models.py
@@ -1,17 +1,26 @@
 from . import db
+import pendulum
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime, default=lambda: pendulum.now("Asia/Kolkata"))
     posts = db.relationship('Post', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime, default=lambda: pendulum.now("Asia/Kolkata"))
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=lambda: pendulum.now("Asia/Kolkata"))
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
--- web/routes.py
+++ web/routes.py
@@ -1,5 +1,5 @@
 from . import db
-from .models import Post
+from .models import Post, User
 from flask_login import login_required, current_user
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 
@@ -15,7 +15,7 @@ def base():
 @login_required
 def home():
     posts = Post.query.all()
-    return render_template("home.html", name=current_user, posts=posts)
+    return render_template("home.html", user=current_user, posts=posts)
 
 @routes.route("/create-post", methods=["GET", "POST"])
 @login_required
@@ -33,3 +33,32 @@ def create_post():
             return redirect(url_for('routes.home'))
 
     return render_template('create_post.html', user=current_user)
+
+@routes.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id =id).first()
+
+    if not post:
+        flash('Post does not exist.', category='error')
+    elif current_user.id != post.author:
+        flash("You don't have the permission to delkete this post.", category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted successfully.', category='success')
+
+    return redirect(url_for('routes.home'))
+
+@routes.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash("No user with that username exists.", category='error')
+        return redirect(url_for('routes.home'))
+
+    posts = user.posts
+
+    return render_template('posts.html', user=current_user, posts=posts, username=username)
